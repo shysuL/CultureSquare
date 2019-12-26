d55cf80b005640342f4105460ef6ef29404d3dc3
godnj <godnj@DESKTOP-3UAVRR3>
2019-12-26 20:14:09 +0900

    마이페이지 메소드 정의, 마이페이지 메인 수정

--- src/main/java/mypage/controller/MypageMainController.java
+++ src/main/java/mypage/controller/MypageMainController.java
@@ -1,15 +1,41 @@
 package mypage.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import user.dto.User_table;
 
 @Controller
 public class MypageMainController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(MypageMainController.class);
+	
 	@RequestMapping(value="/mypage/main", method=RequestMethod.GET)
-	public void mypage() {
+	public void mypage(HttpSession session) {
 		//마이페이지 폼 띄어주는 메소드
+		
+		Object obj = session.getAttribute("userid");
+		
+		logger.info("과연 : " + obj);
+	}
+	
+	@RequestMapping(value="/mypage/main", method=RequestMethod.POST)
+	public void mypageInfo(HttpSession session, Model model, User_table user) {
+		
+		logger.info("마이페이지 뚜둔");
+		
+		//세션에서 로그인되어있는 아이디 가져오기
+		Object obj = session.getAttribute("userid");
+		
+		logger.info("과연 : " + obj);
+		
 	}
 
 }
