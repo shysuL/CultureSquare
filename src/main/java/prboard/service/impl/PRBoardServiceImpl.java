13ff47eb2ea46e7282f4f68e47391c4beff51af0
조홍철 <whghdcjfa@naver.com>
2019-12-27 09:46:32 +0900

    pr보드 일정시간 지났을때 자동으로 유저테이블의 prCnt 0으로 업데이트

--- src/main/java/prboard/service/impl/PRBoardServiceImpl.java
+++ src/main/java/prboard/service/impl/PRBoardServiceImpl.java
@@ -1,16 +1,14 @@
 package prboard.service.impl;
 
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import prboard.controller.PRListController;
 import prboard.dao.face.PRBoardDao;
 import prboard.service.face.PRBoardService;
+import user.dto.User_table;
 
 @Service
 public class PRBoardServiceImpl implements PRBoardService {
@@ -22,24 +20,41 @@ public class PRBoardServiceImpl implements PRBoardService {
 	@Autowired private PRBoardDao prBoardDao;
 	
 	@Override
-	public void getNickName(String nickName) {
-		ScheduledNickname = nickName;
+	public void getNickName(String usernick) {
+		ScheduledNickname = usernick;
 	} 
+	
+	@Override
+	public int getprCntByNickName(String usernick) {
+		User_table user = new User_table();
+		
+		user = prBoardDao.selectprCnt(usernick);
+		
+		return user.getPrcnt();
+		
+	}
 
     /*
      * 5초 마다 실행 => 5000
      * 30초(1분)마다 실행 => 30000
      */
-    @Scheduled(fixedDelay=30000)
+    @Scheduled(fixedDelay=10000)
     public void prCntReset() {
     	
-    	if(ScheduledNickname == null || ScheduledNickname.equals(""))
+    	if(ScheduledNickname == null || ScheduledNickname.equals("") )
     		logger.info("널 : " + ScheduledNickname);
     	else {
-    		logger.info("성공 : " + ScheduledNickname);
-          prBoardDao.updatePRCnt(ScheduledNickname);
+    		
+    		int prCnt = getprCntByNickName(ScheduledNickname);
+    		logger.info("prnct갯수 ? " + prCnt);
+    		
+    		if(prCnt == 0)
+    			logger.info("널은 아닌데 prCnt가 0임 : " + ScheduledNickname);
+    		else {
+    			logger.info("성공 : " + ScheduledNickname);
+    			prBoardDao.updatePRCnt(ScheduledNickname);
+    		}
+    		
     	}
     }
-
-
 }
