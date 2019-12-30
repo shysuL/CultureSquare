89b780e1249d82d21fd5b47b176dadc138ae0e3a
godnj <godnj@DESKTOP-3UAVRR3>
2019-12-30 17:10:58 +0900

    마이페이지 비밀번호 변경 구현중

--- src/main/java/mypage/controller/MypageMainController.java
+++ src/main/java/mypage/controller/MypageMainController.java
@@ -41,28 +41,37 @@ public class MypageMainController {
 	@RequestMapping(value="/mypage/main", method=RequestMethod.POST)
 	public String mypage(User_table user, Model model, HttpSession session) {
 		
-		User_table pwParam = mypageService.getCurrentPwParam(model);
+		user.setUserid(session.getAttribute("userid").toString());
+		user.setUserno((int)session.getAttribute("userno"));
 		
-		pwParam.setUserno((int)session.getAttribute("userno"));
-		pwParam.setUserid(session.getAttribute("userid").toString());
+		User_table userInfo = new User_table();
 		
+		userInfo = mypageService.getFindUserPw(user);
+
 		// 로그인시 입력한 비밀번호를 SHA256으로 암호화
 		String encPw = user.getUserpw();
 		user.setUserpw(PwSha256.userPwEncSHA256(encPw));
 		
 		System.out.println("암호화 : " + encPw);
-		System.out.println("컨트롤러 : " + pwParam);
+		System.out.println("컨트롤러 : " + userInfo);
 		
 		//0과 1로 현재 비밀번호와 입력한 현재 비밀번호가 맞는 지 확인
-		boolean password01 = mypageService.equalsPw(pwParam);
+		boolean password01 = mypageService.equalsPw(userInfo);
+		System.out.println(password01);
 		
-		if(password01) {
-			pwParam.setUserpw((String) model.getAttribute("userpw"));
-			mypageService.modifyUserPassword(pwParam);
-			
+		System.out.println(1);
+		model.addAttribute("userInfo", userInfo);
+		System.out.println(2);
+		
+		if(password01 == true) {
+			System.out.println(3);
+			userInfo.setUserpw((String) model.getAttribute("userpw"));
+			mypageService.modifyUserPassword(user);
+			System.out.println(user.toString());
 			return "/redirect:/main/main";
 			
 		} else {
+			System.out.println(4);
 			return "/redirect:/mypage/main";
 		}
 		
