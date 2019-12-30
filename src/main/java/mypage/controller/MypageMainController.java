203b1c5dbcffcaa414c32c9f722b3f4a9f596532
godnj <godnj@DESKTOP-3UAVRR3>
2019-12-30 17:38:03 +0900

    비밀변경 구현중 ^^

--- src/main/java/mypage/controller/MypageMainController.java
+++ src/main/java/mypage/controller/MypageMainController.java
@@ -46,13 +46,14 @@ public class MypageMainController {
 		
 		User_table userInfo = new User_table();
 		
-		userInfo = mypageService.getFindUserPw(user);
-
+		userInfo = mypageService.getFindUserPw(user); // DB에 있는 비밀번호
+		System.out.println("1 : " + userInfo.getUserpw());
 		// 로그인시 입력한 비밀번호를 SHA256으로 암호화
 		String encPw = user.getUserpw();
-		user.setUserpw(PwSha256.userPwEncSHA256(encPw));
+		user.setUserpw(PwSha256.userPwEncSHA256(encPw)); // 현재비밀번호를 암호환거
 		
 		System.out.println("암호화 : " + encPw);
+		System.out.println("2 : " + userInfo.getUserpw());
 		System.out.println("컨트롤러 : " + userInfo);
 		
 		//0과 1로 현재 비밀번호와 입력한 현재 비밀번호가 맞는 지 확인
@@ -66,8 +67,9 @@ public class MypageMainController {
 		if(password01 == true) {
 			System.out.println(3);
 			userInfo.setUserpw((String) model.getAttribute("userpw"));
-			mypageService.modifyUserPassword(user);
-			System.out.println(user.toString());
+			System.out.println("너가바꾸니?" + (String) model.getAttribute("userpw"));
+			mypageService.modifyUserPassword(userInfo);
+			System.out.println(userInfo.toString());
 			return "/redirect:/main/main";
 			
 		} else {
