a7702430ea3dfa77adf92d29a141708316cdbcc5
shy_L <sooylee628@naver.com>
2019-12-30 17:24:00 +0900

    예술정보게시판 - 주석 수정

--- src/main/java/artboard/controller/ArtboardListController.java
+++ src/main/java/artboard/controller/ArtboardListController.java
@@ -86,6 +86,7 @@ public class ArtboardListController {
 		
 //		logger.info(list.toString());
 				
+		// 모델값으로 cal_year, cal_month를 전달
 		model.addAttribute("nowYear",cal_year);
 		model.addAttribute("nowMonth",cal_month);
 		
@@ -93,12 +94,13 @@ public class ArtboardListController {
 		// 세션에 저장된 userno를 모델로 전달
 		userno.setUserno((Integer)session.getAttribute("userno"));
 		
+		// 세션에 저장된 회원이 예술인인지 조회할 회원정보 조회 
 		Board LoginUser = new Board();
 		LoginUser = pfboardService.getUserByNo(userno);
 		
-		logger.info("LoginUser : " + LoginUser.toString());
+//		logger.info("LoginUser : " + LoginUser.toString());
 		
-		
+		// 조회된 회원정보를 모델로 전달
 		model.addAttribute("LoginUser",LoginUser);
 		
 	}
--- src/main/java/artboard/controller/ArtboardViewController.java
+++ src/main/java/artboard/controller/ArtboardViewController.java
@@ -20,11 +20,15 @@ public class ArtboardViewController {
 	@RequestMapping(value = "/artboard/view", method = RequestMethod.GET)
 	public void pfView(Board bno, Model model) {
 		
+		
+		// 전달받은 파라미터 (boardno)에 해당하는 게시글 상세보기
 		Board viewboard = pfboardService.view(bno);
 		
-		logger.info(viewboard.toString());
+		// 조회된 게시글 모델로 전달
 		model.addAttribute("view", viewboard);
+//		logger.info(viewboard.toString());
 		
+		// 전달받은 파라미터(boardno)에 해당하는 게시글 작성자(userno)로 작성자 정보 조회
 		Board userno = new Board();
 		userno.setUserno(viewboard.getUserno());
 //		System.out.println(userno.getUserno());
@@ -33,6 +37,7 @@ public class ArtboardViewController {
 		
 //		System.out.println("test : " + writer.toString());
 		
+		// 작성자 정보 모델로 전달
 		model.addAttribute("writer", writer);
 		
 	}
