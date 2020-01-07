667f62fc9a05f7ec13708fc9b869bbce8b6a3dc1
조홍철 <whghdcjfa@naver.com>
2020-01-07 15:15:46 +0900

    PR 게시판 답글 작성 완료

--- src/main/java/prboard/dto/Reply.java
+++ src/main/java/prboard/dto/Reply.java
@@ -11,83 +11,71 @@ public class Reply {
 	private int boardno;
 	
 	private String usernick;
-
+	private int maxreplyorder;
 	@Override
 	public String toString() {
 		return "Reply [replyno=" + replyno + ", groupno=" + groupno + ", replyorder=" + replyorder + ", replydepth="
 				+ replydepth + ", recontents=" + recontents + ", replydate=" + replydate + ", userno=" + userno
-				+ ", boardno=" + boardno + ", usernick=" + usernick + "]";
+				+ ", boardno=" + boardno + ", usernick=" + usernick + ", maxreplyorder=" + maxreplyorder + "]";
 	}
-
 	public int getReplyno() {
 		return replyno;
 	}
-
 	public void setReplyno(int replyno) {
 		this.replyno = replyno;
 	}
-
 	public int getGroupno() {
 		return groupno;
 	}
-
 	public void setGroupno(int groupno) {
 		this.groupno = groupno;
 	}
-
 	public int getReplyorder() {
 		return replyorder;
 	}
-
 	public void setReplyorder(int replyorder) {
 		this.replyorder = replyorder;
 	}
-
 	public int getReplydepth() {
 		return replydepth;
 	}
-
 	public void setReplydepth(int replydepth) {
 		this.replydepth = replydepth;
 	}
-
 	public String getRecontents() {
 		return recontents;
 	}
-
 	public void setRecontents(String recontents) {
 		this.recontents = recontents;
 	}
-
 	public String getReplydate() {
 		return replydate;
 	}
-
 	public void setReplydate(String replydate) {
 		this.replydate = replydate;
 	}
-
 	public int getUserno() {
 		return userno;
 	}
-
 	public void setUserno(int userno) {
 		this.userno = userno;
 	}
-
 	public int getBoardno() {
 		return boardno;
 	}
-
 	public void setBoardno(int boardno) {
 		this.boardno = boardno;
 	}
-
 	public String getUsernick() {
 		return usernick;
 	}
-
 	public void setUsernick(String usernick) {
 		this.usernick = usernick;
+	}
+	public int getMaxreplyorder() {
+		return maxreplyorder;
+	}
+	public void setMaxreplyorder(int maxreplyorder) {
+		this.maxreplyorder = maxreplyorder;
 	}
 }
