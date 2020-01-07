91200ec0e8d9e35cd8911fe9a4b955c71b5387b1
강성일 <dudrnwhdk09@gmail.com>
2020-01-07 17:54:01 +0900

    artboard css, 경로 수정

--- src/main/java/artboard/controller/ArtboardListController.java
+++ src/main/java/artboard/controller/ArtboardListController.java
@@ -84,8 +84,7 @@ public class ArtboardListController {
 		
 		model.addAttribute("list", list);
 		
-		logger.info("ㅌㅅㅌ" + list);
-		
+				
 //		logger.info(list.toString());
 				
 		// 모델값으로 cal_year, cal_month를 전달
